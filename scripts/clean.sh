748b0f8e297adb2dc32f8c65ce723bca5510ce12
jimmy.huang <jimmy.huang@104.com.tw>
2020-03-11 15:56:50 +0800

    fix: instance policy and ami

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,8 +4,8 @@
 if [ -f /var/lib/tomcat8/webapps/ROOT.war ]; then
   rm /var/lib/tomcat8/webapps/ROOT.war
 fi
-#刪除 war 自動解開後的 webapp 部署目錄
 
+#刪除 war 自動解開後的 webapp 部署目錄
 if [ -d /var/lib/tomcat8/webapps/ROOT ]; then
   rm -rf /var/lib/tomcat8/webapps/ROOT
 fi
